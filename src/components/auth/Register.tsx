4ff13bb680e13c9eaee88bce95d2d321c2c51a49
UdanDias <udandias892@gmail.com>
2025-06-01 01:33:47 +0530

    completed Register UI

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,6 +1,40 @@
+import { ReactElement, useState } from "react"
 import { Button, Form } from "react-bootstrap"
 
 export const Register = () => {
+    interface Register {
+        firstName: string,
+        lastName: string,
+        email: string,
+        phoneNumber: string,
+        password: string,
+        role: 'ADMIN' | 'STAFF' | 'USER' | ''
+    }
+    const [user, setUser] = useState<Register>({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        password: "",
+        role: ""
+    })
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement>) => {
+        
+        setUser ({ ...user, [e.target.name]: e.target.value })
+    }
+    const handleOnSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        alert("registered user successfully")
+        console.log(user);
+        setUser({
+            firstName: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            password: "",
+            role: ""
+        })
+    }
     return (
         <>
             <h1
@@ -12,41 +46,41 @@ export const Register = () => {
 
 
 
-            <Form className="d-flex flex-column align-items-center mt-5">
+            <Form className="d-flex flex-column align-items-center mt-5" onSubmit={handleOnSubmit}>
                 <div className="w-25">
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter First Name" />
+                        <Form.Control type="text" placeholder="Enter First Name" name="firstName" value={user.firstName} onChange={handleOnChange} />
 
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Last Name" />
+                        <Form.Control type="text" placeholder="Enter Last Name" name="lastName" value={user.lastName} onChange={handleOnChange} />
 
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Email Address" />
+                        <Form.Control type="email" placeholder="Enter Email Address" name="email" value={user.email} onChange={handleOnChange} />
 
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="tel" placeholder="Enter Phone Number" />
+                        <Form.Control type="tel" placeholder="Enter Phone Number" name="phoneNumber" value={user.phoneNumber} onChange={handleOnChange} />
 
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter Password" />
+                        <Form.Control type="password" placeholder="Enter Password" name="password" value={user.password} onChange={handleOnChange} />
 
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formRole">
+                    <Form.Group className="mb-3" controlId="formRole" >
                         <Form.Label>Role</Form.Label>
-                        <Form.Select defaultValue="">
+                        <Form.Select defaultValue="" name="role" value={user.role} onChange={handleOnChange} >
                             <option value="" disabled>Select Role</option>
                             <option value="ADMIN">ADMIN</option>
                             <option value="STAFF">STAFF</option>
